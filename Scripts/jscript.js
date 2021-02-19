33c7cc49a28d339d72ec652951b6b8abaafb2732
Leticia Vieira Costa <lecosta893@gmail.com>
2021-02-19 12:00:34 -0300

    Desenvolvimento Web

---
+++ Scripts/jscript.js
@@ -0,0 +1,34 @@
+function funTrocaTema(par_tema) {
+    CSS_tema.href = "Estilo/CSS_" + par_tema +".css";
+    texto_destaque.innerHTML = par_tema;
+    img_destaque.src = "imagens/" + par_tema + ".png";
+    img_destaque.alt = img_destaque.title = par_tema
+}
+function funBorda() {
+    if (borda.checked) {
+        cor.disabled = false;
+        alert("Agora selecione uma cor!");
+      }   
+      else {
+          texto_destaque.style.borderStyle = "none";
+          cor.options[0].selected ="selected";
+          cor.disabled = true;
+      }
+
+   
+}
+function funCorBorda() {
+    texto_destaque.style.borderStyle = "solid";
+    texto_destaque.style.borderColor = cor.value;
+
+}
+function funSombra() {
+    if (sombra.value == "Adicionar Sombra") {
+     texto_destaque.className = "sombra_texto";
+     sombra.value = "Remove Sombra" 
+    }
+    else {
+        texto_destaque.className = "";
+        sombra.value = "Adicionar Sombra"
+    }
+}
